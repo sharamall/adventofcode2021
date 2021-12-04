cc2b4706f38b394b5a2724dd531450150dc25e95
sharamall <sharamall@hotmail.com>
2021-12-03 20:33:50 -0500

    day2part1

---
+++ day2/include/day2.h
@@ -0,0 +1,14 @@
+//
+// Created by shara on 2021-12-03.
+//
+
+#ifndef ADVENT2021_DAY2_H
+#define ADVENT2021_DAY2_H
+typedef struct {
+    char dir;
+    int distance;
+} input_data;
+input_data *alloc_input(void);
+void dealloc_input(input_data *input);
+
+#endif //ADVENT2021_DAY2_H
---
+++ day2/src/day2part1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "day2.h"
+
+int main(void) {
+    input_data *data = alloc_input();
+    int depth = 0, forward = 0;
+    for (int i = 0; i < 1000; i++) {
+        input_data cur = data[i];
+        if (cur.dir == 'f') {
+            forward += cur.distance;
+        } else if (cur.dir == 'd') {
+            depth += cur.distance;
+        } else if (cur.dir == 'u') {
+            depth -= cur.distance;
+        }
+    }
+    dealloc_input(data);
+    printf("forward %d, depth %d, product %d\n", forward, depth, forward * depth);
+}
