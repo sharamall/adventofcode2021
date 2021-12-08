906f86cd9d897bab747500bcf06375174b459e89
sharamall <sharamall@hotmail.com>
2021-12-08 09:27:16 -0500

    day5part2

--- day5/src/day5part2.c
+++ day5/src/day5part2.c
@@ -14,9 +14,9 @@ int main(void) {
     line_seg *segments = meta->segments;
     int overlaps = 0;
     for (int i = 0; i < 500; i++) {
+        int x_dir = (segments[i].x1 - segments[i].x2) > 0 ? -1 : 1;
+        int y_dir = (segments[i].y1 - segments[i].y2) > 0 ? -1 : 1;
         if (segments[i].x1 == segments[i].x2 || segments[i].y1 == segments[i].y2) {
-            int x_dir = (segments[i].x1 - segments[i].x2) > 0 ? -1 : 1;
-            int y_dir = (segments[i].y1 - segments[i].y2) > 0 ? -1 : 1;
             for (int x = segments[i].x1; (x_dir == 1 ? x <= segments[i].x2 : x >= segments[i].x2); x += x_dir) {
                 for (int y = segments[i].y1; (y_dir == 1 ? y <= segments[i].y2 : y >= segments[i].y2); y += y_dir) {
                     int index = x + y * meta->max_x;
@@ -24,6 +24,15 @@ int main(void) {
                     if (grid[index] == 2) {
                         overlaps++;
                     }
+                }
+            }
+        } else {
+            int count = abs(segments[i].x1 - segments[i].x2);
+            for (int c = 0; c <= count; c++) {
+                int index = (segments[i].x1 + x_dir * c) + (segments[i].y1 + y_dir * c) * meta->max_x;
+                grid[index]++;
+                if (grid[index] == 2) {
+                    overlaps++;
                 }
             }
         }
