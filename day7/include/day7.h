16c821904e0831ba5ae2d04d7e49bcae508fa43a
Marshall.Miller <marshall.miller@flywire.com>
2021-12-10 20:34:43 -0500

    day7part1

---
+++ day7/include/day7.h
@@ -0,0 +1,11 @@
+//
+// Created by shara on 2021-12-10.
+//
+
+#ifndef ADVENT2021_DAY7_H
+#define ADVENT2021_DAY7_H
+
+int *alloc_input(const char *src);
+void dealloc_input(int *input);
+
+#endif //ADVENT2021_DAY7_H
---
+++ day7/src/input.c
@@ -0,0 +1,35 @@
+//
+// Created by shara on 2021-12-10.
+//
+
+//
+// Created by shara on 2021-12-08.
+//
+#include <stdlib.h>
+#include <stdio.h>
+#include "day7.h"
+
+int *alloc_input(const char *src) {
+    FILE *file = fopen(src, "r");
+    if (file) {
+        int *in = malloc(sizeof(int) * 1000);
+        for (int i = 0; i < 1000; i++) {
+            if (fscanf(file, "%d%*c", in + i) != 1) {
+                printf("bad input at %d\n", i);
+                dealloc_input(in);
+                fclose(file);
+                return 0;
+            }
+        }
+        fclose(file);
+        return in;
+    }
+
+    return 0;
+}
+
+void dealloc_input(int *input) {
+    if (input) {
+        free(input);
+    }
+}
