9c15a6e651f7ec660c1cc88039bb8943e8503237
sharamall <sharamall@hotmail.com>
2021-12-21 17:28:33 -0500

    day21part1

---
+++ day21/include/day21.h
@@ -0,0 +1,12 @@
+//
+// Created by shara on 2021-12-21.
+//
+
+#ifndef ADVENT2021_DAY21_H
+#define ADVENT2021_DAY21_H
+
+#include <stdlib.h>
+
+int64_t read_input(const char *file);
+
+#endif //ADVENT2021_DAY21_H
