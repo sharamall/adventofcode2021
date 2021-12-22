60dde0ac1407b206fa1c65b72b2725afa67cb187
sharamall <sharamall@hotmail.com>
2021-12-22 12:33:43 -0500

    day21part2

--- day21/src/day21part2.c
+++ day21/src/day21part2.c
@@ -10,7 +10,6 @@
 
 typedef struct {
     int64_t p1_wins, p2_wins;
-
 } results;
 
 typedef struct {
@@ -21,15 +20,15 @@ typedef struct {
 
 results ******create_cache() {
     results ******cache = zalloc(sizeof(results *****) * 2);
-    cache[0] = zalloc(sizeof(results ****) * 11);
-    cache[1] = zalloc(sizeof(results ****) * 11);
+    cache[0] = zalloc(sizeof(results ****) * 10);
+    cache[1] = zalloc(sizeof(results ****) * 10);
 
     return cache;
 }
 
 void free_cache_p2_score(results **r) {
     if (r) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 31; i++) {
             free(r[i]);
         }
         free(r);
@@ -38,7 +37,7 @@ void free_cache_p2_score(results **r) {
 
 void free_cache_p1_score(results ***r) {
     if (r) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 31; i++) {
             free_cache_p2_score(r[i]);
         }
         free(r);
@@ -83,40 +82,52 @@ cached_result get_cached_item(results ******r, game g) {
     results ****p1_pos = this_turn[g.p1_pos];
 
     if (!p1_pos) {
-        this_turn[g.p1_pos] = zalloc(sizeof(results ***) * 11);
+        this_turn[g.p1_pos] = zalloc(sizeof(results ***) * 10);
         p1_pos = this_turn[g.p1_pos];
         existed = 0;
     }
     results ***p2_pos = p1_pos[g.p2_pos];
     if (!p2_pos) {
-        p1_pos[g.p2_pos] = zalloc(sizeof(results **) * 11);
+        p1_pos[g.p2_pos] = zalloc(sizeof(results **) * 31);
         p2_pos = p1_pos[g.p2_pos];
         existed = 0;
     }
     results **p1_score = p2_pos[g.p1_score];
     if (!p1_score) {
-        p2_pos[g.p1_score] = zalloc(sizeof(results *) * 32);
+        p2_pos[g.p1_score] = zalloc(sizeof(results *) * 31);
         p1_score = p2_pos[g.p1_score];
         existed = 0;
     }
     results *p2_score = p1_score[g.p2_score];
     if (!p2_score) {
-        p1_score[g.p2_score] = zalloc(sizeof(results) * 32);
+        p1_score[g.p2_score] = zalloc(sizeof(results));
         p2_score = p1_score[g.p2_score];
         existed = 0;
     }
-    if ((size_t)p2_score < 0x1000) {
-        printf("whack\n");
-    }
 
-    results *p = p2_score + g.p2_score;
-    if ((size_t)p == 0x123) {
-        printf("heck\n");
-    }
-    return (cached_result){.r=p, .existed=existed};
+    return (cached_result){.r=p2_score, .existed=existed};
 }
 
-void run_game(results ******r, game g) {
+results run_game(results ******r, game g) {
+    results *****turn = r[g.p1_turn];
+    if (turn[g.p1_pos]) {
+        results ****p1_pos = turn[g.p1_pos];
+        if (p1_pos[g.p2_pos]) {
+            results ***p2_pos = p1_pos[g.p2_pos];
+            if (p2_pos[g.p1_score]) {
+                results **p1_score = p2_pos[g.p1_score];
+                if (p1_score[g.p2_score]) {
+                    results *p2_score = p1_score[g.p2_score];
+                    if (p2_score) {
+                        results result = *p2_score;
+                        return result;
+                    }
+                }
+            }
+        }
+    }
+
+    results conclusion = {.p1_wins = 0, .p2_wins = 0};
     for (int x = 1; x <= 3; x++) {
         for (int y = 1; y <= 3; y++) {
             for (int z = 1; z <= 3; z++) {
@@ -124,46 +135,37 @@ void run_game(results ******r, game g) {
                 game new_game = g;
                 if (new_game.p1_turn) {
                     new_game.p1_pos += roll;
-                    if (new_game.p1_pos > 10) {
-                        new_game.p1_pos %= 10;
-                    }
-                    new_game.p1_score += new_game.p1_pos;
+                    new_game.p1_pos %= 10; // 0-9 instead of 1-10
+                    new_game.p1_score += new_game.p1_pos + 1;
                     new_game.p1_turn = 0;
-                    cached_result res = get_cached_item(r, new_game);
                     if (new_game.p1_score >= SCORE_LIMIT) {
-                        res.r->p1_wins++;
+                        conclusion.p1_wins++;
                     } else {
-                        if (res.existed) {
-//                            cached_result res1 = get_cached_item(r, g);
-//                            res1.r->p1_wins += res1.r->p1_wins;
-//                            res1.r->p2_wins += res1.r->p2_wins;
-                        } else {
-                            run_game(r, new_game);
-                        }
+                        results recursive_wins = run_game(r, new_game);
+                        conclusion.p1_wins += recursive_wins.p1_wins;
+                        conclusion.p2_wins += recursive_wins.p2_wins;
                     }
                 } else {
                     new_game.p2_pos += roll;
-                    if (new_game.p2_pos > 10) {
-                        new_game.p2_pos %= 10;
-                    }
-                    new_game.p2_score += new_game.p2_pos;
+                    new_game.p2_pos %= 10; // 0-9 instead of 1-10
+                    new_game.p2_score += new_game.p2_pos + 1;
                     new_game.p1_turn = 1;
-                    cached_result res = get_cached_item(r, new_game);
                     if (new_game.p2_score >= SCORE_LIMIT) {
-                        res.r->p2_wins++;
+                        conclusion.p2_wins++;
                     } else {
-                        if (res.existed) {
-//                            cached_result res1 = get_cached_item(r, g);
-//                            res1.r->p1_wins += res1.r->p1_wins;
-//                            res1.r->p2_wins += res1.r->p2_wins;
-                        } else {
-                            run_game(r, new_game);
-                        }
+                        results recursive_wins = run_game(r, new_game);
+                        conclusion.p1_wins += recursive_wins.p1_wins;
+                        conclusion.p2_wins += recursive_wins.p2_wins;
                     }
                 }
             }
         }
     }
+    cached_result res = get_cached_item(r, g);
+    res.r->p1_wins = conclusion.p1_wins;
+    res.r->p2_wins = conclusion.p2_wins;
+
+    return conclusion;
 }
 
 /*
@@ -177,13 +179,13 @@ void run_game(results ******r, game g) {
 
 int main(void) {
     int64_t in = read_input("../../day21/resources/input.txt");
-    int p1 = 4;// in >> 32;
-    int p2 = 8;//in & 0xFFFFFFFF;
+    int p1 = (in >> 32) - 1;
+    int p2 = (in & 0xFFFFFFFF) - 1;
     results ******r = create_cache();
     game g = {p1, p2, 0, 0, 1};
-    run_game(r, g);
+    results final_result = run_game(r, g);
 
-    printf("p1 wins %ld, p2 win %ld", r[1][p1][p2][0][0]->p1_wins, r[1][p1][p2][0][0]->p2_wins);
+    printf("p1 wins %ld, p2 win %ld", final_result.p1_wins, final_result.p2_wins);
     free_cache(r);
     return 0;
 }
