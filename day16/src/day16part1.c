77d0b8167218b7420a2ee3359b035bc337f37849
Marshall.Miller <marshall.miller@flywire.com>
2021-12-22 14:36:43 -0500

    day16part1

--- day16/src/day16part1.c
+++ day16/src/day16part1.c
@@ -5,6 +5,72 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void read_operator(int *in, int *i, int *version);
+
+void read_packet(int *in, int *i, int *version) {
+    int packet_version = in[*i] << 2 | in[*i + 1] << 1 | in[*i + 2] << 0;
+    *version += packet_version;
+    *i += 3;
+    int packet_type_id = in[*i] << 2 | in[*i + 1] << 1 | in[*i + 2] << 0;
+    *i += 3;
+    if (packet_type_id == 4) {
+        // literal
+        list *l = 0;
+        int first;
+        do {
+            first = in[*i];
+            int val = in[*i + 1] << 3 | in[*i + 2] << 2 | in[*i + 3] << 1 | in[*i + 4] << 0;
+            *i += 5;
+            push_back(&l, val);
+        } while (first);
+        int val = 0;
+        for (int j = 0; j < l->size; j++) {
+            val <<= 4;
+            val |= (int)l->items[j];
+        }
+        printf("got literal %d, remain %d, i %d\n", val, *i % 4, *i);
+    } else {
+        read_operator(in, i, version);
+    }
+
+}
+
+void read_operator(int *in, int *i, int *version) {
+    //operator
+    int len_bit = in[*i];
+    *i += 1;
+    if (len_bit) {
+        //11 bits, total number of subpackets
+        int length = 0;
+        for (int j = 0; j < 11; j++) {
+            length <<= 1;
+            length |= in[*i + j];
+        }
+        *i += 11;
+        printf("operator packet, num packets %d\n", length);
+        int sub_packets = 0;
+        for (int j = 0; j < length; j++) {
+            printf("reading sub packet at off %d, num %d\n", *i, sub_packets++);
+            read_packet(in, i, version);
+        }
+    } else {
+        //15 bits to read sub packets
+        int length = 0;
+        for (int j = 0; j < 15; j++) {
+            length <<= 1;
+            length |= in[*i + j];
+        }
+        *i += 15;
+        printf("operator packet, bit length %d\n", length);
+        int initial_i = *i;
+        int sub_packets = 0;
+        while (*i - initial_i < length) {
+            printf("reading sub packet at off %d, num %d\n", *i, sub_packets++);
+            read_packet(in, i, version);
+        }
+    }
+}
+
 int main(void) {
     int *in = alloc_input("../../day16/resources/input.txt");
     int i = 0;
@@ -13,54 +79,12 @@ int main(void) {
     }
     i = 0;
     printf("\n");
-    while (i < LENGTH * 4) {
-        int packet_version = in[i] << 2 | in[i + 1] << 1 | in[i + 2] << 0;
-        i += 3;
-        int packet_type_id = in[i] << 2 | in[i + 1] << 1 | in[i + 2] << 0;
-        i += 3;
-        if (packet_type_id == 4) {
-            // literal
-            list *l = 0;
-            int first;
-            do {
-                first = in[i];
-                int val = in[i + 1] << 3 | in[i + 2] << 2 | in[i + 3] << 1 | in[i + 4] << 0;
-                i += 5;
-                push_back(&l, val);
-            } while (first);
-            int val = 0;
-            for (int j = 0; j < l->size; j++) {
-                val <<= 4;
-                val |= (int)l->items[j];
-            }
-            printf("got literal %d, remain %d, i %d\n", val, i % 4, i);
-            break;
-        } else {
-            //operator
-            int len_bit = in[i++];
-            if (len_bit == 0) {
-                //15 bits
-                int length = 0;
-                for (int j = 0; j < 15; j++) {
-                    length <<= 1;
-                    length |= in[i + j];
-                }
-                printf("operator packet, length %d\n", length);
-                break;
-            } else {
-                //11 bits
-                int length = 0;
-                for (int j = 0; j < 11; j++) {
-                    length <<= 1;
-                    length |= in[i + j];
-                }
-                printf("operator packet, length %d\n", length);
-                break;
-            }
-        }
-        printf("packet version %d, packet_type_id %d\n", packet_version, packet_type_id);
-
-    }
+    int version = 0;
+    read_packet(in, &i, &version);
+//    while (i < LENGTH * 4) {
+//        printf("packet version %d, packet_type_id %d\n", packet_version, packet_type_id);
+//    }
+    printf("version_numbers %d\n", version);
     dealloc_input(in);
     return 0;
 }
